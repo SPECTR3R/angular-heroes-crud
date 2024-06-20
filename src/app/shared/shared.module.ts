b77c4313290ea9f43f413dc3e58b4f1bdd3888d0
Armando del Río <armando@nolte.io>
2024-06-20 12:26:03 -0600

    feat: add modules, pages and routing

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
+
+@NgModule({
+  declarations: [Error404PageComponent],
+  imports: [Error404PageComponent, CommonModule],
+})
+export class SharedModule {}
