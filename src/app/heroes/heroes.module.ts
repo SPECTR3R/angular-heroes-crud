58165349e41252aeeb646b502be75c710f4f14c8
Armando del Río <armando@nolte.io>
2024-06-20 14:50:46 -0600

    feat: add login page and sidenav

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,7 @@ import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
     NewHeroPageComponent,
     SearchPageComponent,
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, MaterialModule],
 })
 export class HeroesModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Error404PageComponent } from './pages/error-404-page/error-404-page.component';
 
 @NgModule({
   declarations: [Error404PageComponent],
-  imports: [Error404PageComponent, CommonModule],
+  exports: [Error404PageComponent],
 })
 export class SharedModule {}
